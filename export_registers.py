74699ea0b498efbb0fb3358d7830c18f4290f36e
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2021-03-10 08:47:57 +0000

    add records

---
+++ export_registers.py
@@ -0,0 +1,19 @@
+import json
+import requests
+from pathlib import Path
+
+registers = requests.get('https://register.register.gov.uk/records.json')
+json = registers.json()
+beta_registers=list(json.keys())
+FORMATS = ['json', 'csv', 'rsf']
+
+for register in beta_registers:
+    Path(register).mkdir(parents=True, exist_ok=True)
+    for format in FORMATS:
+        if format == 'rsf':
+            url = f'https://{register}.register.gov.uk/download-rsf'
+        else:
+            url = f'https://{register}.register.gov.uk/records.{format}?page-size=5000'
+        r = requests.get(url)
+        with open(f"{register}/records.{format}", "w") as f:
+            f.write(r.text)
